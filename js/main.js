388de67f5a0c1360a420066468acfd529c26aeaa
Jesabel Kern <jesabelkern@gmail.com>
2023-07-01 17:51:38 -0300

    add styles css and to declare const

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* BOTONES DEL NAV */
+const buttonImage = document.querySelector(".image")
+
+const buttonText = document.querySelector(".text")
+
+const buttonMode = document.querySelector(".dark-mode")
+
+
+/* SELECCIÓN DE LOS ASIDE DE IMAGEN Y TEXTO */
+const asideEditImage = document.querySelector(".edit-image")
+
+const asideEditText = document.querySelector(".edit-text")
